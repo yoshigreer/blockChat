67dadf6059f745820e3c54a77af11d07cb874f71
yoshigreer <yoshigreer@gmail.com>
2016-12-29 19:22:42 -0700

    done with mentor session

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,28 @@
 (function() {
-  function HomeCtrl(Room) {
+  function HomeCtrl(Room, $uibModal) {
     this.rooms = Room.all;
+      this.addRo
+
+      this.openModal = function () {
+        //take room name and create room and add to array
+        console.log("open");
+        var modal = $uibModal.open({
+          templateUrl: "/templates/modal.html",
+          controller: function($scope) {
+            console.log('controller of modal');
+
+          }
+        });
+        //Room.all.$add({})
+      };
+
+      this.cancel = function () {
+        //close modal window
+        $uibModal.dismiss();
+      };
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 })();
